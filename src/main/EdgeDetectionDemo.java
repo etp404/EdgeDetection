ceedca6813c39c25a8f2f404eee059d2ca1613a7
matt <mattmould86@gmail.com>
2014-08-11 20:55:23 +0100

    Added scaling. It's a bit messier than I'd like: I'm dividing elementwise by a matrix as I can't find the method for dividing by a scalar.

--- src/main/EdgeDetectionDemo.java
+++ src/main/EdgeDetectionDemo.java
@@ -21,25 +21,26 @@ public class EdgeDetectionDemo {
             double sigma = lambda;
             Size size = new Size(2*sigma, 2*sigma);
             Mat outputForThisScale = new Mat(inputImage64.height(), inputImage64.width(), inputImage64.depth());
-            for (double orientation = 0; orientation < 2*Math.PI; orientation+=Math.PI/8) {
+            for (double orientation = 0; orientation < 2*Math.PI; orientation+=Math.PI/4) {
                 Mat kernel = Imgproc.getGaborKernel(size, sigma, orientation, lambda, 1);
-                Mat output = new Mat();
-                Imgproc.filter2D(inputImage64, output, inputImage64.depth(), kernel);
+                Mat output = new Mat(inputImage64.height(), inputImage64.width(), inputImage64.depth());
+                Imgproc.filter2D(inputImage64, output, -1, kernel);
                 Core.max(output, outputForThisScale, outputForThisScale);
+
+                Mat scaleMatrix = new Mat(inputImage.height(),inputImage.width(), CvType.CV_64F, new Scalar(Core.minMaxLoc(output).maxVal/255.0));
+                Core.divide(output, scaleMatrix, output); //Not ideal: I only want to divide by a scalar.
+
+                String outputFilename = "resources/separate/wavelength_" + String.valueOf(lambda) + "_orientation_" + String.valueOf(orientation/(Math.PI/8)) + "_output.png";
+                Highgui.imwrite(outputFilename, output);
+
             }
-            String outputFilename = "resources/output_" + String.valueOf(lambda) + ".png";
-            Highgui.imwrite(outputFilename, result);
+            String outputFilename = "resources/wavelength_" + String.valueOf(lambda) + "_output.png";
+            Highgui.imwrite(outputFilename, outputForThisScale);
             Core.add(result, outputForThisScale, result);
         }
 
-        Core.MinMaxLocResult maxMin = Core.minMaxLoc(result);
-        System.out.println(maxMin.maxVal);
-
-        Core.convertScaleAbs(result, result);
-
-        maxMin = Core.minMaxLoc(result);
-        System.out.println(maxMin.minVal);
-        System.out.println(maxMin.maxVal);
+        Mat scaleMatrix = new Mat(inputImage.height(),inputImage.width(), CvType.CV_64F, new Scalar(Core.minMaxLoc(result).maxVal/255.0));
+        Core.divide(result, scaleMatrix, result); //Not ideal: I only want to divide by a scalar.
 
         String outputFilename = "resources/output.png";
         Highgui.imwrite(outputFilename, result);
