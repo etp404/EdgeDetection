ac739b1075e12a6e279d76e5654a3ca05d06c3a2
Matt Mould <matthew.mould@bbc.co.uk>
2014-08-15 19:46:41 +0100

    Added cropping logic and put border on to represent headline panel.

--- src/main/java/uk/co/bbc/EdgeDetectionDemo.java
+++ src/main/java/uk/co/bbc/EdgeDetectionDemo.java
@@ -8,7 +8,7 @@ import org.apache.commons.io.FilenameUtils;
 import java.io.File;
 
 public class EdgeDetectionDemo {
-    private static final int HEADLINE_PANEL_WIDTH = 1000;
+    private static final int HEADLINE_PANEL_WIDTH = 1200;
 
     public static void main(String[] args) {
         System.loadLibrary("opencv_java249");
@@ -17,33 +17,45 @@ public class EdgeDetectionDemo {
         File folder = new File("resources/input");
         File[] listOfFiles = folder.listFiles();
 
-        for (int i = 0; i<4; i++) {
+        for (int i = 0; i<5
+				                              ; i++) {
             File inputImageFile = listOfFiles[i];
-            System.out.println(i + " of " + listOfFiles.length);
+            System.out.println(i+1 + " of " + listOfFiles.length);
             Mat inputImage = Highgui.imread(inputImageFile.getAbsolutePath(), Highgui.CV_LOAD_IMAGE_GRAYSCALE);
             Mat result = getEdgeMap(inputImage);
             int bestLeftEdge = getBestLeftEdge(result);
             Mat scaleMatrix = new Mat(inputImage.height(),inputImage.width(), CvType.CV_64F, new Scalar(Core.minMaxLoc(result).maxVal/255.0));
             Core.divide(result, scaleMatrix, result); //Not ideal: I only want to divide by a scalar.
 
-            String inputFilename = "resources/results/" + FilenameUtils.removeExtension(inputImageFile.getName()) + "_input.png";
-            Core.rectangle(inputImage, new Point(bestLeftEdge,0), new Point (bestLeftEdge + HEADLINE_PANEL_WIDTH, inputImage.height()), new Scalar(0), 10);
-            Highgui.imwrite(inputFilename, inputImage);
+//            String outputFilename = "resources/results/" + FilenameUtils.removeExtension(inputImageFile.getName()) + "_output.png";
+//            Highgui.imwrite(outputFilename, result);
 
+			Mat originalImage = Highgui.imread(inputImageFile.getAbsolutePath(), Highgui.CV_LOAD_IMAGE_COLOR);
 
-            String outputFilename = "resources/results/" + FilenameUtils.removeExtension(inputImageFile.getName()) + "_output.png";
-            Highgui.imwrite(outputFilename, result);
+			String inputFilename = "resources/results/" + FilenameUtils.removeExtension(inputImageFile.getName()) + "_input.png";
+			Highgui.imwrite(inputFilename, originalImage);
+
+			String originalImageWithHeadline = "resources/results/" + FilenameUtils.removeExtension(inputImageFile.getName()) +
+					"_withHeadline.png";
+
+			Mat imCrop = originalImage.submat(0, originalImage.height(), bestLeftEdge, bestLeftEdge + originalImage.width()
+													  - HEADLINE_PANEL_WIDTH).clone();
+
+			Mat output = new Mat();
+			Imgproc.copyMakeBorder(imCrop, output, 0, 0, HEADLINE_PANEL_WIDTH, 0, 0);
+
+			Highgui.imwrite(originalImageWithHeadline, output);
         }
     }
 
     private static int getBestLeftEdge(Mat result) {
         System.out.println("Optimizing position of headline.");
         int bestLeftEdge = 0;
-        Double lowestInterestScore = null;
-        for (int x=0; x<result.width()-HEADLINE_PANEL_WIDTH; x++) {
-            double interestScore = Core.sumElems(result.colRange(x, x + HEADLINE_PANEL_WIDTH)).val[0];
-            if (lowestInterestScore == null || interestScore < lowestInterestScore){
-                lowestInterestScore = interestScore;
+        Double highestInterestScore = 0.0;
+        for (int x=0; x<HEADLINE_PANEL_WIDTH; x++) {
+            double interestScore = Core.sumElems(result.colRange(x, x + result.width() - HEADLINE_PANEL_WIDTH)).val[0];
+            if (interestScore > highestInterestScore){
+				highestInterestScore = interestScore;
                 bestLeftEdge = x;
             }
         }
